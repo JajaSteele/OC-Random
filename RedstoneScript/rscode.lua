a8956d54e47f384a742dc3175b50bf373271f76b
Jaja Steele <superjaja05@gmail.com>
2021-12-22 11:44:25 +0100

    Update rscode.lua

--- RedstoneScript/rscode.lua
+++ RedstoneScript/rscode.lua
@@ -3,6 +3,10 @@ local rs = c.redstone
 local ev = require("event")
 local shell = require("shell")
 
+t1 = io.open("/home/.shrc","w")
+t1:write("rscode.lua")
+t1:close()
+
 while true do
     local _, _, side, oldV, newV = ev.pull("redstone_changed")
 
