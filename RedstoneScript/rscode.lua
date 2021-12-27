6d80b5755c6f40fa0e49990ab7654e2a861d7197
Jaja Steele <superjaja05@gmail.com>
2021-12-27 15:28:38 +0100

    Update rscode.lua

--- RedstoneScript/rscode.lua
+++ RedstoneScript/rscode.lua
@@ -2,18 +2,27 @@ local c = require("component")
 local rs = c.redstone
 local ev = require("event")
 local shell = require("shell")
+local colors = require("colors")
 
 t1 = io.open("/home/.shrc","w")
 t1:write("rscode.lua")
 t1:close()
 
 while true do
-    local _, _, side, oldV, newV = ev.pull("redstone_changed")
-
-    if oldV == 0 and newV > 0 then
-        shell.execute("/home/rs/rsON.lua")
-    end
-    if oldV > 0 and newV == 0 then
-        shell.execute("/home/rs/rsOFF.lua")
+    local _, _, side, oldV, newV, colorV = ev.pull("redstone_changed")
+    if colorV == nil then
+        if oldV == 0 and newV > 0 then
+            shell.execute("/home/rs/rsON.lua")
+        end
+        if oldV > 0 and newV == 0 then
+            shell.execute("/home/rs/rsOFF.lua")
+        end
+    else
+        if oldV == 0 and newV > 0 then
+            shell.execute("/home/rs/rsON"..colors[colorV]..".lua")
+        end
+        if oldV > 0 and newV == 0 then
+            shell.execute("/home/rs/rsOFF"..colors[colorV]..".lua")
+        end
     end
 end
