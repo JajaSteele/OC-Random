d88a68ba9ec1e5625c5001752b8aec1558b755ff
Jaja Steele <superjaja05@gmail.com>
2022-02-22 11:34:43 +0100

    Update sgauto.lua

--- StargateStuff/sgauto.lua
+++ StargateStuff/sgauto.lua
@@ -3,12 +3,21 @@ local sg = cp.stargate
 local ct = require("computer")
 local colors = require("colors")
 local term = require("term")
+local internet = require("internet")
+local keyboard = require("keyboard")
 local g = cp.gpu
+
+if io.open("/lib/json.lua","r") then
+    json = require("json")
+else
+    os.execute("wget https://github.com/rxi/json.lua/raw/master/json.lua /lib/json.lua")
+    json = require("json")
+end
 
 function waitState(x1,t1)
     repeat
         local state1,chevron1,direction1 = sg.stargateState()
-        os.sleep(0.1)
+        os.sleep(0.15)
     until state1 == x1
     os.sleep(t1)
 end
@@ -16,9 +25,17 @@ end
 function waitChevr(x1,t1)
     repeat
         local state1,chevron1,direction1 = sg.stargateState()
-        os.sleep(0.1)
+        os.sleep(0.15)
     until chevron1 == x1
     os.sleep(t1)
+end
+
+function getTime(timezone)
+    headers = {["accept"] = "application/json"}
+    t1 = internet.request("https://www.timeapi.io/api/Time/current/zone?timeZone="..timezone,nil,headers,"GET")
+    res = ""
+    for chunk in t1 do res = res..chunk end
+    return json.decode(res)
 end
 
 function cprint(t1)
@@ -58,9 +75,8 @@ function cprint(t1)
     term.setCursor(oldX,oldY2+1)
 end
 
-t1 = io.open("/home/.shrc","w")
-t1:write("sgauto.lua")
-t1:close()
+os.sleep(0.5)
+if keyboard.isAltDown() then return end
 
 while true do
     local state,chevron,direction = sg.stargateState()
@@ -70,7 +86,8 @@ while true do
         else
             dirtext = "#0xFF4444 [<--]"
         end
-        cprint("#0xFFFFFF [|-|] #0xBBBBBB Connection Detected!\n      Address: "..sg.remoteAddress().."\n      Direction: "..direction.." "..dirtext)
+        time = getTime("Europe/Amsterdam")
+        cprint("#0xFFFFFF [|-|] #0xBBBBBB <"..time["hour"]..":"..time["minute"]..":"..time["seconds"].."> Connection Detected!\n      Address: "..sg.remoteAddress().."\n      Direction: "..direction.." "..dirtext)
         ct.beep(300,0.1)
         os.sleep(0.15)
         ct.beep(560,0.1)
@@ -79,11 +96,11 @@ while true do
         waitChevr(string.len(sg.remoteAddress())-3,0.1)
         sg.closeIris()
         waitState("Connected",0.01)
-        cprint("#0x44FF44 [<->] Connection Opened.")
+        cprint("#0x44FF44 [<->] <"..time["hour"]..":"..time["minute"]..":"..time["seconds"].."> Connection Opened.")
         os.sleep(2.25)
         sg.openIris()
         waitState("Idle",0.01)
-        cprint("#0xFF4444 [>-<] Connection Ended.")
+        cprint("#0xFF4444 [>-<] <"..time["hour"]..":"..time["minute"]..":"..time["seconds"].."> Connection Ended.")
         ct.beep(300,0.2)
         os.sleep(0.15)
         ct.beep(150,0.2)
