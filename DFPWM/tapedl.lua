b0c019ace262eeba3adbcc4dc2b3386989652293
Jaja Steele <superjaja05@gmail.com>
2025-09-26 08:57:28 +0200

    Update tapedl.lua

--- DFPWM/tapedl.lua
+++ DFPWM/tapedl.lua
@@ -4,13 +4,35 @@ local term = require("term")
 local internet = component.internet
 local tape = component.tape_drive
 
+local dl_bar_end = {
+    {c="▉", t=0},
+    {c="▊", t=0},
+    {c="▋", t=0},
+    {c="▌", t=0},
+    {c="▍", t=0},
+    {c="▎", t=0},
+    {c="▏", t=0},
+}
+local diff = 1/(#dl_bar_end+1)
+for k,v in ipairs(dl_bar_end) do
+    v.t = diff*(#dl_bar_end-(k-1))
+end
+local function getDlFooter(decimal)
+    for k,v in ipairs(dl_bar_end) do
+        if v.t <= decimal then
+            return v.c
+        end
+    end
+    return ""
+end
+
 term.clear()
 
 print("Enter Youtube ID:")
 local id = io.read()
 
 print("High Quality Mode? (y/n):")
-print("(will double file size)")
+print("(will double file size, requires x2 playback speed)")
 local hq = io.read()
 if hq == "true" or hq == "y" or hq == "1" or hq == "" then
     hq = true
@@ -45,6 +67,11 @@ local count = 0
 while true do
     local chunk, reason = req.read(size)
     if not chunk then
+        if reason then
+            print("ERROR! "..reason)
+        else
+            print("Download finished!")
+        end
         break
     elseif count >= tape_size then
         print("WARNING! Reached end of tape!")
@@ -58,10 +85,14 @@ while true do
         term.write(string.format("Progress: %.1f%%", perc*100))
         term.setCursor(1, dl_y+1)
         term.clearLine()
-        term.write(string.rep("=", w*(perc)))
+        term.write("["..string.rep("█", math.floor((w-2)*(perc))))
+        local decimal = (w-2)*(perc) - math.floor((w-2)*(perc))
+        term.write(getDlFooter(decimal))
+        term.setCursor(w, dl_y+1)
+        term.write("]")
         --print(string.format("%.1f%%", (count/size)*100))
     else
-        
+        os.sleep()
     end
 end
 
